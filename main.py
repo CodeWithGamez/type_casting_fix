59392d98c590a15f1f74583bd6a20a5eac89f0d3
CodeWithGamez <codewithgamez@gmail.com>
2020-08-23 03:49:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Fix my mistakes of using TYPE CASTING
+
+
+
+#1 Change the string to have it print the number five 
+
+number = "five"
+print(int(number))
+
+
+#2 Make both numbers be the same type to make it print
+
+print(4 + "5")
+
+
+
+#3 Add both numbers
+
+
+print( "40" + 240 )
+
+
+
+
